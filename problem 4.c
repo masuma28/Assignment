e0dbfe29346d28a98401969d3edb3bcff2766897
masuma28 <aktermasuma2024@gmail.com>
2024-11-14 12:01:52 +0600

    Add files via upload

---
+++ problem 4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int a,b,c;
+printf("please enter 3 digit\n");
+scanf("%d%d%d",&a,&b,&c);
+if (a>b)
+printf("a is largset\n");
+else
+ printf("b is largest\n");
+return 0;
+}
